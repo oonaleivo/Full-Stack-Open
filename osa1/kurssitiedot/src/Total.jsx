1a9dfc95ae6a5ad43b1e9cc3d6e9e95fc0285aa4
Oona Leivo <oonamarialeivo@gmail.com>
2024-01-29 14:24:37 +0200

    1.3 kurssitiedot step3

--- osa1/kurssitiedot/src/Total.jsx
+++ osa1/kurssitiedot/src/Total.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Total = ({exercises1, exercises2, exercises3}) => {
+const Total = ({totalExercises}) => {
   return (
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <p>Number of exercises {totalExercises}
+    </p>
   );
 };  
 
